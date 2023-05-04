36d81910d461c89b87733a45386b2a92c1f286ed
bdalberson <Brian.alberson@gmail.com>
2023-05-03 20:29:23 -0700

    about to seed the db, wish me luck

--- config/connection.js
+++ config/connection.js
@@ -15,20 +15,3 @@ const sequelize = process.env.JAWSDB_URL
 module.exports = sequelize;
 
 
-
-
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: '127.0.0.1',
-//     dialect: 'mysql',
-//     port: 3306,
-//   }
-// );
-
-// module.exports = sequelize;
