71c1222f1b0297de10b48195ab27b3fb8c4c58ba
bdalberson <Brian.alberson@gmail.com>
2023-05-04 20:29:09 -0700

    tags seem to work correctly.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,21 +10,43 @@ router.get('/', async (req, res) => {
   res.json(tags);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tag = await Tag.findByPk(req.params.id, {
+    include: [{ model: Product }],
+  });
+  res.json(tag)
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async(req, res) => {
+  const tag = await Tag.create(req.body);
+    res.status(200).json(tag);
+    console.log("new tag created")
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(() => {
+    res.json({ response: "Tag updated successfully" });
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+
+      
+    }
+  }).then(() => {
+    res.json({ alert: "Tag deleted successfully" });
+  })
 });
 
 module.exports = router;
