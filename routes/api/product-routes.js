5ff20fbc787fe77dd552224abc4470b238f71e9f
bdalberson <Brian.alberson@gmail.com>
2023-05-04 22:07:52 -0700

    Geeze there were a bunch of paths I hadn't updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -52,46 +52,16 @@ router.post('/', async (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
-  // update product data
-  Product.update(req.body, {
+router.put('/:id', async(req, res) => {
+   await Product.update(req.body, {
     where: {
       id: req.params.id,
     },
   })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+    .then((updatedProduct) => {
+      res.json(updatedProduct);
     })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
-});
+  });
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
@@ -99,6 +69,7 @@ router.delete('/:id', async (req, res) => {
     where: { id: req.params.id },
   });
   res.json({ alert: 'Category deleted' });
+  console.log("Product updated!")
 });
 
 module.exports = router;
